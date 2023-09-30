dfe2b9852abb133effb6fda53855047d8e62edc8
Andrew112236 <102410886+Andrew112236@users.noreply.github.com>
2023-09-30 17:04:08 +0300

    react-api_code

---
+++ src/API_links/Api.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios';
+const API_KEY = '6761a75cfe3ce8a6cf2b591c7da9567e';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+
+// API function for searching movies
+
+async function searchMovies(query) {
+  try {
+    const API_URL = `search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('There is a error in search movies of', error);
+    throw error;
+  }
+}
+
+// API function for movie details
+
+async function getMovieDetails(id) {
+  try {
+    const API_URL = `movie/${id}?api_key=${API_KEY}&language=en-US`;
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('There is a error in search movies details of', error);
+    throw error;
+  }
+}
+
+// API function for movie trendings
+
+async function getTrending() {
+  try {
+    const API_URL = `trending/movie/day?api_key=${API_KEY}`;
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('There is a error in searching trendings of', error);
+    throw error;
+  }
+}
+
+// API function for movie reviews
+
+async function getReviews(id) {
+  try {
+    const API_URL = `movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`;
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('There is a error in searching reviews of', error);
+    throw error;
+  }
+}
+
+// API function for movie credits
+
+async function getMovieCredits(id) {
+  try {
+    const API_URL = `movie/${id}/credits?api_key=${API_KEY}&language=en-US`;
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('There is a error in searching reviews of', error);
+    throw error;
+  }
+}
+
+module.exports = searchMovies;
+module.exports = getMovieDetails;
+module.exports = getTrending;
+module.exports = getReviews;
+module.exports = getMovieCredits;
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+function Searchbar({ onSubmit }) {
+  const [query, setQuery] = React.useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(query);
+  };
+
+  const handleInputChange = e => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <header className={styles.Searchbar}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          className={styles.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search Movies"
+          value={query}
+          onChange={handleInputChange}
+        />
+        <button type="submit" className={styles.button}>
+          <span className={styles.button_label}>Search</span>
+        </button>
+      </form>
+    </header>
+  );
+}
+
+export default Searchbar;
