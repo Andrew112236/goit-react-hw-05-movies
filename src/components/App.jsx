e68c8f8eebcc48e6ceb133a839b6812269394a17
Andrew112236 <102410886+Andrew112236@users.noreply.github.com>
2023-10-04 23:08:29 +0300

    react-app-text

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,24 @@
-import SearchBar from './SearchBar/SearchBar';
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+const Home = lazy(() => import('../Pages/Home/Home'));
+const MovieDetails = lazy(() => import('../Pages/MovieDetails/MovieDetails'));
+const Movies = lazy(() => import('../Pages/Movies/Movies'));
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <SearchBar />
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
   );
 };
