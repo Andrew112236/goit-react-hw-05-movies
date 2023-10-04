f5154346abe99b10d32607b5413e627a6404576f
Andrew112236 <102410886+Andrew112236@users.noreply.github.com>
2023-10-04 21:43:20 +0300

    react-pages

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import { MovieLink, MovieList, Title } from './Home.module.css';
+import { getTrending } from '../../API_links/Api';
+import { Loader } from 'react-loader-spinner';
+import { useLocation } from 'react-router-dom';
+
+function Home() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    async function fetchData() {
+      try {
+        const data = await getTrending();
+        setTrendingMovies(data.results);
+        setIsLoading(false);
+      } catch (error) {
+        console.log('There is and error:', error);
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <Title>Top Movies for today</Title>
+      <MovieList>
+        {trendingMovies.map(movie => (
+          <li key={movie.id}>
+            <MovieLink to={`/movies/${movie.id}`} state={{ from: location }}>
+              {movie.original_title || movie.name}
+            </MovieLink>
+          </li>
+        ))}
+        {isLoading && <Loader />}
+      </MovieList>
+    </main>
+  );
+}
+
+export default Home;
---
+++ src/Pages/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation, useParams, Link } from 'react-router-dom';
+import { getMovieDetails } from '../../API_links/Api';
+import {
+  Button,
+  GenresList,
+  InfoBox,
+  LinkBack,
+  MovieBox,
+  MovieInfo,
+  Title,
+} from './MovieDetails.module.css';
+
+function MovieDetails() {
+  const { movieIdParam } = useParams();
+  const [movieDetail, setMovieDetail] = useState({});
+  const location = useLocation();
+
+  useEffect(() => {
+    getMovieDetails(movieIdParam).then(data => setMovieDetail(data));
+  }, [movieIdParam]);
+
+  const { original_title, overview, genres, poster_path, vote_average } =
+    movieDetail;
+  const score = vote_average * 10;
+  const scoreToFixed = score.toFixed(2);
+
+  return (
+    <main>
+      <Button type="button">
+        <LinkBack to={location.state?.from ?? '/'}>Go back</LinkBack>
+      </Button>
+      <MovieBox className="MovieBox">
+        <img
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w300${poster_path}`
+              : `http://www.suryalaya.org/images/no_image.jpg`
+          }
+          width={320}
+          height={380}
+          loading="lazy"
+          alt="poster"
+        />
+        <MovieInfo>
+          <Title>{original_title}</Title>
+          <h3>User score: {scoreToFixed}%</h3>
+          <h3>Overview</h3>
+          <p>{overview}</p>
+          <h3>Genres</h3>
+          <GenresList>
+            {genres &&
+              genres.length &&
+              genres.map(({ id, name }) => <li key={id}>{name}</li>)}
+          </GenresList>
+        </MovieInfo>
+      </MovieBox>
+      <InfoBox>
+        <h4>Additional information</h4>
+        <ul>
+          <li>
+            <Link
+              to={{
+                pathname: '/cast',
+                state: { ...location.state },
+              }}
+            >
+              Cast
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={{
+                pathname: '/reviews',
+                state: { ...location.state },
+              }}
+            >
+              Reviews
+            </Link>
+          </li>
+        </ul>
+      </InfoBox>
+    </main>
+  );
+}
+
+export default MovieDetails;
